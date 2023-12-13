bcb9c5bb8f57a62f1fa12c625810fffc48c1b16a
mrrobotoe <aldo.martinez@my.utsa.edu>
2023-12-13 09:04:26 -0600

    initial commit, cloned from api-app

---
+++ app/core/models.py
@@ -0,0 +1,49 @@
+"""Database models."""
+
+from django.db import models
+from django.contrib.auth.models import (
+    AbstractBaseUser,
+    BaseUserManager,
+    PermissionsMixin,
+)
+
+
+class UserManager(BaseUserManager):
+    """Manager for users."""
+
+    def create_user(
+        self, email, password=None, **extra_fields
+    ):  # keyword args for extra_fields
+        """Create, save and return a new user."""
+        if not email:
+            raise ValueError("User must have an email address.")
+        user = self.model(email=self.normalize_email(email), **extra_fields)
+        # uses hash process with set_password method
+        user.set_password(password)
+        user.save(using=self._db)
+
+        return user
+
+    def create_superuser(self, email, password):
+        """Create and return a new superuser."""
+        user = self.create_user(email, password)
+        user.is_staff = True
+        user.is_superuser = True
+        user.save(using=self._db)
+
+        return user
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    """User in the system."""
+
+    email = models.EmailField(max_length=255, unique=True)
+    name = models.CharField(max_length=255)
+    is_active = models.BooleanField(default=True)
+    # if user can log into django admin
+    is_staff = models.BooleanField(default=False)
+
+    # assigned UserManager to user custom class
+    objects = UserManager()
+
+    USERNAME_FIELD = "email"
